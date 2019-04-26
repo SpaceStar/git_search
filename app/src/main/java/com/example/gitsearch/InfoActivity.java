e0e1ceb1d0c9294e33a63d2390b0512f12092d5d
SpaceStar <spacestar.ru@gmail.com>
2019-04-27 00:33:55 +0300

    use Glide library

--- app/src/main/java/com/example/gitsearch/InfoActivity.java
+++ app/src/main/java/com/example/gitsearch/InfoActivity.java
@@ -1,17 +1,11 @@
 package com.example.gitsearch;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.constraint.Group;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.InputStream;
+import com.bumptech.glide.Glide;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -20,41 +14,11 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-        final ImageView userPhoto = findViewById(R.id.infoUserPhoto);
-        final ProgressBar progressBar = findViewById(R.id.infoDownloadProgress);
-        final Group content = findViewById(R.id.infoContent);
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String url = getIntent().getStringExtra("photoUrl");
-                try {
-                    InputStream in = new java.net.URL(url).openStream();
-                    final Bitmap bmImage = BitmapFactory.decodeStream(in);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            userPhoto.setImageBitmap(bmImage);
-                            progressBar.setVisibility(View.GONE);
-                            content.setVisibility(View.VISIBLE);
-                        }
-                    });
-                } catch (Exception e) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(InfoActivity.this, "error", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        progressBar.setVisibility(View.GONE);
-                    }
-                });
-            }
-        }).start();
+        ImageView userPhoto = findViewById(R.id.infoUserPhoto);
+        String url = getIntent().getStringExtra("photoUrl");
+        Glide.with(this)
+                .load(url)
+                .into(userPhoto);
 
         TextView name = findViewById(R.id.infoUsername);
         TextView repository = findViewById(R.id.infoRepositoryName);
--- app/src/main/java/com/example/gitsearch/RepositoryAdapter.java
+++ app/src/main/java/com/example/gitsearch/RepositoryAdapter.java
@@ -2,42 +2,36 @@ package com.example.gitsearch;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.gitsearch.models.SearchResults;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.RepositoryViewHolder> {
     private List<SearchResults.Item> repositories;
-    private List<Bitmap> photos;
     private final Activity mActivity;
     private View.OnClickListener repoClick;
 
     public RepositoryAdapter(Activity activity) {
         this.repositories = new ArrayList<>();
-        this.photos = new ArrayList<>();
         this.mActivity = activity;
         repoClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ImageView userPhoto = v.findViewById(R.id.itemUserPhoto);
                 TextView username = v.findViewById(R.id.itemUsername);
                 TextView repositoryName = v.findViewById(R.id.itemRepositoryName);
 
                 Intent intent = new Intent(mActivity.getApplicationContext(), InfoActivity.class);
-                intent.putExtra("photoUrl", (String) userPhoto.getTag());
+                intent.putExtra("photoUrl", (String) username.getTag());
                 intent.putExtra("username", username.getText());
                 intent.putExtra("repositoryName", repositoryName.getText());
                 mActivity.startActivity(intent);
@@ -47,14 +41,11 @@ public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.Re
 
     public void clearItems() {
         repositories.clear();
-        photos.clear();
         notifyDataSetChanged();
     }
 
     public void addItems(List<SearchResults.Item> items) {
         repositories.addAll(items);
-        for (int i = 0; i < items.size(); i++)
-            photos.add(null);
         notifyDataSetChanged();
     }
 
@@ -63,8 +54,7 @@ public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.Re
     public RepositoryViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
         v.setOnClickListener(repoClick);
-        RepositoryViewHolder repositoryViewHolder = new RepositoryViewHolder(v);
-        return repositoryViewHolder;
+        return new RepositoryViewHolder(v);
     }
 
     @Override
@@ -77,38 +67,12 @@ public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.Re
         SearchResults.Item repository = repositories.get(i);
         repositoryViewHolder.getRepositoryName().setText(repository.getName());
         repositoryViewHolder.getUsername().setText(repository.getOwner().getLogin());
-        repositoryViewHolder.getUserPhoto().setImageResource(android.R.color.transparent);
-        final String url = repository.getOwner().getAvatarUrl();
-        repositoryViewHolder.getUserPhoto().setTag(url);
-
-        final Bitmap bmImage = photos.get(i);
-        if (bmImage != null) {
-            mActivity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    repositoryViewHolder.getUserPhoto().setImageBitmap(bmImage);
-                }
-            });
-        } else {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        InputStream in = new java.net.URL(url).openStream();
-                        final Bitmap bmImage = BitmapFactory.decodeStream(in);
-                        photos.set(repositoryViewHolder.getAdapterPosition(), bmImage);
-                        mActivity.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                repositoryViewHolder.getUserPhoto().setImageBitmap(bmImage);
-                            }
-                        });
-                    } catch (Exception e) {
-                        Log.e("error", e.getMessage());
-                    }
-                }
-            }).start();
-        }
+        String url = repository.getOwner().getAvatarUrl();
+        repositoryViewHolder.getUsername().setTag(url);
+        Glide.with(mActivity)
+                .load(url)
+                .circleCrop()
+                .into(repositoryViewHolder.getUserPhoto());
     }
 
     @Override
